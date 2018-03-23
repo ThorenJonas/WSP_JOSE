a4f8a1ea53aca07221e9a4bbdd60cacaf22e4259
Jonas Segedahl <jonas@Jonass-MacBook-Air.local>
2018-03-23 11:07:04 +0100

    array

--- Laboration_PHP/Beginning/resources/functions/model.php
+++ Laboration_PHP/Beginning/resources/functions/model.php
@@ -5,32 +5,32 @@
 // ändra alla keys some '0' an
 $arr =array (
       '0' =>array (
-            'slug' => 'value1-1',
-            'title' => 'value1-2',
-            'author' => 'value1-3',
-            'date' => 'value1-4',
-            'text' => 'value1-5'
+            'slug' => 'forsta-inlagget',
+            'title' => 'Första inlägget',
+            'author' => 'Gugge',
+            'date' => '	2015-01-01	',
+            'text' => 'Här kommer det första inlägget i sin helhet. Välkommen till Labb 3! Här övar vi på PHP för att bli duktiga webbserverprogrammerare. Detta är tredje labben och andra labben i en serie labbar som tillsammans bygger ihop detta projekt. Ett enkelt PHP-projekt men väl strukturerat och genomtänkt konstruerat.'
       ),
       '1' =>array (
-            'slug' => 'value1-1',
-            'title' => 'value1-2',
-            'author' => 'value1-3',
-            'date' => 'value1-4',
-            'text' => 'value1-5'
+            'slug' => 'snart-ar-det-var',
+            'title' => 'Snart är det vår',
+            'author' => 'Gugge',
+            'date' => '2015-02-24',
+            'text' => 'Snart är det vår! Då övar vi på PHP för att bli duktiga webbserverprogrammerare. Detta är tredje labben och andra labben i en serie labbar som tillsammans bygger ihop detta projekt. Ett enkelt PHP-projekt men väl strukturerat och genomtänkt konstruerat.'
       ),
       '2' =>array (
-            'slug' => 'value1-1',
-            'title' => 'value1-2',
-            'author' => 'value1-3',
-            'date' => 'value1-4',
-            'text' => 'value1-5'
+            'slug' => 'robin-presenterar-sig',
+            'title' => 'Robin presenterar sig',
+            'author' => 'Robin',
+            'date' => '2015-02-25',
+            'text' => 'Robin är en trevlig prick som gärna övar på PHP för att som du bli en duktig webbserverprogrammerare. Detta är tredje labben och andra labben i en serie labbar som tillsammans bygger ihop detta projekt. Ett enkelt PHP-projekt men väl strukturerat och genomtänkt konstruerat.'
       ),
       '3' =>array (
-            'slug' => 'value1-1',
-            'title' => 'value1-2',
-            'author' => 'value1-3',
-            'date' => 'value1-4',
-            'text' => 'value1-5'
+            'slug' => 'senaste-inlagget',
+            'title' => 'Senaste inlägget',
+            'author' => 'Robin',
+            'date' => '	2015-02-26',
+            'text' => 'Här kommer senaste inlägget i sin helhet. Välkommen till Labb 3! Här övar vi på PHP för att bli duktiga webbserverprogrammerare. Detta är tredje labben och andra labben i en serie labbar som tillsammans bygger ihop detta projekt. Ett enkelt PHP-projekt men väl strukturerat och genomtänkt konstruerat.'
       )
 );
 ?>
